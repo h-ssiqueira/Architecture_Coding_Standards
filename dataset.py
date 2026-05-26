cc1c872d19269dec0cacded7eca60db50688f0e2
Henrique <henriquesartorisiqueira@hotmail.com>
2026-05-25 21:33:15 -0300

    updating dataset extraction

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 import json, networkx as nx, pandas as pd, re, requests
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 status = {
@@ -13,53 +14,63 @@ status = {
     'Unknown': '#000000'                   # Black
 }
 
-htmlText = requests.get('https://www.rfc-editor.org/search/rfc_search_detail.php?page=All&pub_date_type=any&sortkey=Number&sorting=ASC').content
-soup = BeautifulSoup(htmlText, 'lxml')
-
-page = soup.find("table", class_="gridtable")
-i = 0
+i = 1
 items = []
-for tr in page.find_all("tr"):
-    if i == 0:
-        i += 1
-        continue
-    j = 0
-    content = []
-    for td in tr.find_all("td"):
-        if j == 0 or j == 2 or j == 4:
-            content.append(td.text.strip())
-        elif j == 5:
-            if td.text.strip() == '':
-                content.append('')
-            else:
-                content.append(td.text.replace('Errata, ','').replace('Errata','').strip())
-        elif j == 6:
-            content.append(td.text.split('(')[0].strip())
-        j += 1
-    items.append(content)
+while(True):
+    jsoncontent = requests.post(url = 'https://typesense.ietf.org/multi_search?x-typesense-api-key=0C8Exv9grP2li1fwQeg34nPtKfccC3Qa',
+                                data = f'''{{
+  "searches": [{{
+    "preset": "red",
+    "sort_by": "publicationDate:desc",
+    "collection": "docs",
+    "q": "*",
+    "facet_by": "area.full,authors.name,flags.hiddenDefault,group.full,publicationDate,status.name,stream.name",
+    "max_facet_values": 1,
+    "page": {i},
+    "per_page": 250
+  }}]
+}}''').json()
+    total = jsoncontent['results'][0]['found']
+    
+    pages = jsoncontent['results'][0]['hits']
+    for page in pages:
+        info = ''
+        if page['document']["obsoletedBy"]:
+            info += "Obsoleted by " + ", ".join(f'RFC {item}' for item in page["document"]["obsoletedBy"])
+        if page['document']["updatedBy"]:
+            info += 'Updated by ' + ", ".join(f'RFC {item}' for item in page["document"]["updatedBy"])
+        items.append([
+            page['document']['rfcNumber'],
+            page['document']['title'],
+            datetime.fromtimestamp(page['document']['publicationDate']),
+            info,
+            page['document']['status']['name']
+        ])
+    if(i * 250 >= total):
+        break
+    print(f'{i*250}/{total}')
     i += 1
 df = pd.DataFrame(items, columns=['RFC','Title','Date','Info','Status'])
-for i, r in df.iterrows():
-    if r['Status'] == None:
-        df.iloc[i] = ['RFC\u00a01685','Writing X.400 O/R Names','August 1994','','Informational']
-df["RFC"] = df["RFC"].str.extract(r'(?i)(RFC\s*\d+)', expand=False)
+
+print(f'Total RFCs: {total}')
+print(f'Total pages: {i}')
 
 updatesPattern = r'(Updates|Updated\sby)\s(RFC\s\d+(?:,\sRFC\s\d+)*)'
 obsoletesPattern = r'(Obsoletes|Obsoleted\sby)\s(RFC\s\d+(?:,\sRFC\s\d+)*)'
 
 G = nx.Graph()
 for i, r in df.iterrows():
-    G.add_node(r["RFC"], title=f'{r["Title"]}', date=f'{r["Date"]}', status=f'{r["Status"]}', color=status[r['Status']], size=20)
+    G.add_node(f'RFC {r["RFC"]}', title=f'{r["Title"]}', date=f'{r["Date"]}', status=f'{r["Status"]}', color=status[r['Status']], size=20)
 for _, row in df.iterrows():
     if row['Info'] != '':
         updates = re.findall(updatesPattern, row['Info'])
         obsoletes = re.findall(obsoletesPattern, row['Info'])
         for i in updates:
             for j in i[1].split(', '):
-                G.add_edge(row['RFC'], j)
+                G.add_edge(f'RFC {row["RFC"]}', j)
         for i in obsoletes:
             for j in i[1].split(', '):
-                G.add_edge(row['RFC'], j)
+                G.add_edge(f'RFC {row["RFC"]}', j)
 graph_data = {
     'nodes': [{'id': node, **data} for node, data in G.nodes(data=True)],
     'edges': [{'source': source, 'target': target} for source, target in G.edges()]
